69f403b00731e1cecbea1d0ad20efeb1d5ebefe3
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-09-26 06:55:06 -0500

    formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel()
+    output: "server",
+    adapter: vercel(),
 });
